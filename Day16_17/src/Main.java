8b5b15f9d084da5c491344ba737647138a41d2c2
AbinayaSelvam <sabinaya352@gmail.com>
2026-05-25 11:18:52 +0530

    added the heapsort

--- Day16_17/src/Main.java
+++ Day16_17/src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args)
     {
         System.out.println("Welcome to the submission sorting algorithm");
+        int[] salaries={20000,34000,56000,23000,90000};
+        HeapSort.heapSort(salaries);
+        System.out.println(Arrays.toString(salaries));
     }
 }
