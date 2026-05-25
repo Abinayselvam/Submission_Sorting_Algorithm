8126adfac25cc8f074fa41c67052fb6a9c6e15ff
AbinayaSelvam <sabinaya352@gmail.com>
2026-05-25 11:11:01 +0530

    Added the Selection Sort

--- Day16_17/src/Main.java
+++ Day16_17/src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args)
     {
         System.out.println("Welcome to the submission sorting algorithm");
+        int[] score={70,40,35,100,39,90,30};
+        SelectionSort.selectionSort(score);
+        System.out.println(Arrays.toString(score));
     }
 }
